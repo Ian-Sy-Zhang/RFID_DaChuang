874b5ff5c28eb267d789370b3605df0f0b9e925b
0APPTI0 <2458530429@qq.com>
2020-06-07 11:49:48 +0800

    新增了一些修正数据的sql

---
+++ BackEnd_Code/sql/rfid_spot.sql
@@ -0,0 +1,2 @@
+INSERT INTO rfid.spot (SpotId, SpotName, SpotX, SpotY) VALUES (1, 'spot1', 0, 0);
+INSERT INTO rfid.spot (SpotId, SpotName, SpotX, SpotY) VALUES (2, 'spot2', 1, 1);
---
+++ BackEnd_Code/sql/rfid_transport.sql
@@ -0,0 +1,3 @@
+INSERT INTO rfid.transport (TransportId, CAS_id, batchId, Num, State) VALUES (1, 1, 1, 1, 1);
+INSERT INTO rfid.transport (TransportId, CAS_id, batchId, Num, State) VALUES (2, 2, 1, 1, 1);
+INSERT INTO rfid.transport (TransportId, CAS_id, batchId, Num, State) VALUES (3, 1, 2, 1, 1);
